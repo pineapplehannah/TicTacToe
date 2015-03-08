a22624123e4eced71aa1c09d31d47a6624ae30e4
Hannah Stephenson <littlehannah96@gmail.com>
2015-03-08 22:20:22 +0000

    Outlined how the game will be played

---
+++ tictactoe.rb
@@ -0,0 +1,7 @@
+#prompt user to pick a square
+#replace that square in the board with the players stamp
+#check against an array of winning combinations
+#if no winning combinations || if number on spaces left on board >= 1
+#prompt the next player to take a turn
+#else, the game is over 
+# if there is a winning combination, determine the winning player and print "Congratulations #{winning player}. You win!"
