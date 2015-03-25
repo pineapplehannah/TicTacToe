d8ef4abdf88fef393e6ee347c8f83f5d90c9bdda
Hannah Stephenson <littlehannah96@gmail.com>
2015-03-25 12:02:40 +0000

    Got my board string working

--- tictactoe.rb
+++ tictactoe.rb
@@ -9,7 +9,6 @@
 class Game
 
 	@spaces = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-	@board = " #{@spaces[0]} | #{@spaces[1]} | #{@spaces[2]}\n --------- \n #{@spaces[3]} | #{@spaces[4]} | #{@spaces[5]}\n --------- \n #{@spaces[6]} | #{@spaces[7]} | #{@spaces[8]}"
 	@player_a = 'X'
 	player_b = 'O'
 
@@ -17,13 +16,13 @@ class Game
 	
 	def self.play
 		puts "Player A, pick the space on your board where you'd like to mark your #{@player_a}"
-		puts @board
+		puts " #{@spaces[0]} | #{@spaces[1]} | #{@spaces[2]}\n --------- \n #{@spaces[3]} | #{@spaces[4]} | #{@spaces[5]}\n --------- \n #{@spaces[6]} | #{@spaces[7]} | #{@spaces[8]}"
 		choice = gets.chomp
 		foo = @spaces.index(choice.to_i)
 		@spaces[foo] = 'X'
-
-		puts @spaces.inspect
+		@board = " #{@spaces[0]} | #{@spaces[1]} | #{@spaces[2]}\n --------- \n #{@spaces[3]} | #{@spaces[4]} | #{@spaces[5]}\n --------- \n #{@spaces[6]} | #{@spaces[7]} | #{@spaces[8]}"
 		puts @board
+
 
 		#if @spaces.values_at(0, 1, 2 || 3, 4, 5 || 6, 7, 8 || 0, 3, 6 || 1, 4, 7 || 2, 5, 8 || 0, 4, 8 || 2, 4, 6) == ['X', 'X', 'X'] || ['O', 'O', 'O']
 		#	puts "game is over. Winner is #{@player_a or @player_b}"
