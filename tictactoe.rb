c38a7d3bf70cc9851161a66d91f0fc239ffc228c
Hannah Stephenson <littlehannah96@gmail.com>
2015-03-25 11:53:00 +0000

    Got my function to update @spaces array working

--- tictactoe.rb
+++ tictactoe.rb
@@ -8,25 +8,30 @@
 
 class Game
 
-	spaces = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-	board = " #{spaces[0]} | #{spaces[1]} | #{spaces[2]}\n --------- \n #{spaces[3]} | #{spaces[4]} | #{spaces[5]}\n --------- \n #{spaces[6]} | #{spaces[7]} | #{spaces[8]}"
-	player_a = 'X'
+	@spaces = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+	@board = " #{@spaces[0]} | #{@spaces[1]} | #{@spaces[2]}\n --------- \n #{@spaces[3]} | #{@spaces[4]} | #{@spaces[5]}\n --------- \n #{@spaces[6]} | #{@spaces[7]} | #{@spaces[8]}"
+	@player_a = 'X'
 	player_b = 'O'
 
 	winning_combos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
 	
-	def play(spaces, board)
-		puts "Player A, pick the space on your board where you'd like to mark your #{player_a}"
-		puts board
+	def self.play
+		puts "Player A, pick the space on your board where you'd like to mark your #{@player_a}"
+		puts @board
 		choice = gets.chomp
-		foo = spaces.index(chosen_space.to_i)
-		spaces[foo] = player_a 
+		foo = @spaces.index(choice.to_i)
+		@spaces[foo] = 'X'
 
-		if spaces.values_at(0, 1, 2 || 3, 4, 5 || 6, 7, 8 || 0, 3, 6 || 1, 4, 7 || 2, 5, 8 || 0, 4, 8 || 2, 4, 6) == ['X', 'X', 'X'] || ['O', 'O', 'O']
-			puts "game is over. Winner is #{player_a or player_b}"
-		else
-			# play_on (do this loop again. Make this a loop!)
-		end
+		puts @spaces.inspect
+		puts @board
+
+		#if @spaces.values_at(0, 1, 2 || 3, 4, 5 || 6, 7, 8 || 0, 3, 6 || 1, 4, 7 || 2, 5, 8 || 0, 4, 8 || 2, 4, 6) == ['X', 'X', 'X'] || ['O', 'O', 'O']
+		#	puts "game is over. Winner is #{@player_a or @player_b}"
+		#else
+		#	# play_on (do this loop again. Make this a loop!)
+		#end
 	end
 
-end	
+end
+
+Game.play
